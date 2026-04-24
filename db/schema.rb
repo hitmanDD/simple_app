da67be3d43a4cedf62fea37bf76ac7ecf9fe4503
hitmanDD <hitman_tt@mail.ru>
2026-04-24 12:33:14 -0700

    Добавили активацию юезра.миграции и настойки почты

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2026_04_23_201214) do
+ActiveRecord::Schema[7.0].define(version: 2026_04_24_183439) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,9 @@ ActiveRecord::Schema[7.0].define(version: 2026_04_23_201214) do
     t.string "password_digest"
     t.text "bio"
     t.boolean "admin", default: false
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
