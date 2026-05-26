034959b3180e2c0a252a04e05002de04e5642738
hitmanDD <hitman_tt@mail.ru>
2026-05-26 11:35:41 -0700

    Сделали Helper по методу Хартла для генерации заголовков страниц

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,7 @@
 module ApplicationHelper
+  include Pagy::Frontend # Подключаем фронтенд Pagy для вывода кнопок страниц
+
+  # Метод Хартла для генерации заголовков страниц
   def full_title(page_title = '')
     base_title = "Ruby on Rails Tutorial"
     if page_title.empty?
